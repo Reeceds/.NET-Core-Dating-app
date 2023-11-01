8ccebdaed44bebdb91d7233b810eb6eeabfa4251
Reeceds <32424143+Reeceds@users.noreply.github.com>
2023-11-01 13:14:20 -0400

    Extending the API

---
+++ API/DTOs/PhotoDto.cs
@@ -0,0 +1,8 @@
+﻿namespace API.DTOs;
+
+public class PhotoDto
+{
+    public int Id { get; set; }
+    public string Urk { get; set; }
+    public bool IsMain { get; set; }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,7 @@
 using API;
+using API.Data;
 using API.Extensions;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,5 +22,19 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+
+using var scope = app.Services.CreateScope(); // Gives you access to all of the services within this Program class
+var services = scope.ServiceProvider;
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync(); // If you drop DB, this gives a new DB with refereshed seed data
+    await Seed.SeedUsers(context);
+}
+catch (Exception ex)
+{
+    var logger = services.GetService<ILogger<Program>>();
+    logger.LogError(ex, "An error occurred during migration");
+}
 
 app.Run();
